1a05b34d214aea6c31459b26985681280844d15b
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2024-06-15 20:38:08 -0300

    feat-Register activities and grades-requirements 1 and 2

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,8 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +12,23 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<EvaluativeActivity> evaluationsList = new ArrayList<>();
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int evaluationsQuantity = Integer.parseInt(scanner.nextLine());
+
+    for (int index = 1; index <= evaluationsQuantity; index += 1) {
+      EvaluativeActivity activity = new EvaluativeActivity();
+      System.out.println("Digite o nome da atividade " + index + ":");
+      activity.setEvaluationName(scanner.nextLine());
+      System.out.println("Digite o peso da atividade " + index + ":");
+      activity.setEvaluationWeight(Integer.parseInt(scanner.nextLine()));
+      System.out.println("Digite a nota obtida para " + activity.getEvaluationName() + ":");
+      activity.setEvaluationGrade(Integer.parseInt(scanner.nextLine()));
+
+      evaluationsList.add(activity);
+    }
+    scanner.close();
   }
 }
---
+++ src/main/java/com/trybe/java/regraprogressao/EvaluativeActivity.java
@@ -0,0 +1,75 @@
+package com.trybe.java.regraprogressao;
+
+/**
+ * The type Evaluative activity.
+ */
+public class EvaluativeActivity {
+
+  /**
+   * The Evaluation weight.
+   */
+  int evaluationWeight;
+  /**
+   * The Evaluation grade.
+   */
+  int evaluationGrade;
+  /**
+   * The Evaluation name.
+   */
+  String evaluationName;
+
+  /**
+   * Gets evaluation grade.
+   *
+   * @return the evaluation grade
+   */
+  public int getEvaluationGrade() {
+    return evaluationGrade;
+  }
+
+  /**
+   * Sets evaluation grade.
+   *
+   * @param evaluationGrade the evaluation grade
+   */
+  public void setEvaluationGrade(int evaluationGrade) {
+    this.evaluationGrade = evaluationGrade;
+  }
+
+  /**
+   * Gets evaluation weight.
+   *
+   * @return the evaluation weight
+   */
+  public int getEvaluationWeight() {
+    return evaluationWeight;
+  }
+
+  /**
+   * Sets evaluation weight.
+   *
+   * @param evaluationWeight the evaluation weight
+   */
+  public void setEvaluationWeight(int evaluationWeight) {
+    this.evaluationWeight = evaluationWeight;
+  }
+
+  /**
+   * Gets evaluation name.
+   *
+   * @return the evaluation name
+   */
+  public String getEvaluationName() {
+    return evaluationName;
+  }
+
+  /**
+   * Sets evaluation name.
+   *
+   * @param evaluationName the evaluation name
+   */
+  public void setEvaluationName(String evaluationName) {
+    this.evaluationName = evaluationName;
+  }
+
+}
